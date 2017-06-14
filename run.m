240c4951cd466404af2096378093175a3c48c05f
foca13 <guillemguigo@gmail.com>
2017-06-14 10:22:15 +0200

    Add files via upload

--- run.m
+++ run.m
@@ -1,15 +1,15 @@
 function [] = run(pos_body,pos_head,vec_tail,vec,con,per,w,t_w,s,t_run,t)
 a=getGlobalx;
 if t_run<10
-    pos_head=pos_head+0.1*vec;
     pos_body=pos_body+0.1*vec;
+    pos_head=pos_body+vec;
     if pos_head(1)>=650
         pos_head(1)=650;
         pos_body(2)=pos_body(2)*(vec(2)/vec(2)*2);
         pos_head(2)=pos_head(2)+(vec(2)/vec(2)*2);
     end
-    if pos_head(1)<=0
-        pos_head(1)=0;
+    if pos_head(1)<=1
+        pos_head(1)=1;
         pos_body(2)=pos_body(2)+(vec(2)/vec(2)*2);
         pos_head(2)=pos_head(2)+(vec(2)/vec(2)*2);
     end
@@ -18,8 +18,8 @@ if t_run<10
         pos_body(1)=pos_body(1)+(vec(1)/vec(1)*2);
         pos_head(1)=pos_head(1)+(vec(1)/vec(1)*2);
     end
-    if pos_head(2)<=0
-        pos_head(2)=0;
+    if pos_head(2)<=1
+        pos_head(2)=1;
         pos_body(1)=pos_body(1)+(vec(1)/vec(1)*2);
         pos_head(1)=pos_head(1)+(vec(1)/vec(1)*2);
     end
@@ -28,73 +28,40 @@ if t_run<10
     con=a(floor(pos_head(1)),floor(pos_head(2)));
     t=t+1;
     t_run=t_run+1;
-    plot(pos_body(1),pos_body(2),'ob'); hold on;
+    %plot(pos_body(1),pos_body(2),'ob'); hold on;
     run(pos_body,pos_head,vec_tail,vec,con,per,w,t_w,s,t_run,t);
 end
-
-% if w==0
-%     pos_body=pos_body+0.1*vec;
-%     pos_head=pos_head+0.1*vec;
-%     vec_tail=vec;
-%     per=cat(2,per,perception(arena,pos_head,con));
-%     %vec=pos_head-pos_body+0.1*vec;
-%     if rand <0.1
-%         w=w+1;
-%     end
-% else if w==1
-%         pos_head=weathervane(1);
-%         per=cat(2,per,weathervane(2));
-%         pos_body=pos_body+0.1*vec;
-%     end
-% end
-% angle=acosd(dot(vec,vec_tail)/(norm(vec)*norm(vec_tail))); 
-% if w == 1
-%     if abs(angle) < 5
-%         if rand<0.5
-%             vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)];
-%             s=s+1;
-%         else
-%             vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
-%         end
-%     else if abs(angle) < 17
-%             if s==1
-%                 vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)]; 
-%             else
-%                 vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
-%             end
-%         else
-%             if s==1
-%                 s=0;
-%                 vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
-%             else 
-%                 s=1;
-%                 vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)];
-%             end
-%          end
-%     end
-%     pos_head=pos_head+0.1*vec;
-%     pos_body=pos_body+0.1*vec;
-%     per=cat(2,per,perception(arena,pos_head,con));
-%     t_w=t_w+1;
-%     weathervane_termination_base=2;
-%     t_weathervane_kernel=0;
-%     for i = 0:t_w
-%         if i<=1
-%             t_weathervane_kernel=t_weathervane_kernel+per(t-i+1)*85;
-%         else if i<=10
-%             t_weathervane_kernel=t_weathervane_kernel+per(t-i+1)*(-5);
-%             end
-%         end
-%     end
-%     r_weathervane_terminate=weathervane_termination_base+t_weathervane_kernel;
-%     p_weathervane_terminate=r_weathervane_terminate*0.1;
-%     if rand<p_weathervane_terminate
-%     w=0;
-%     end
-% end
-plot(pos_body(1),pos_body(2),'ob');hold on;
-pos_head=pos_head+0.1*vec;
+if rand<0.1
+    w=1;
+end
+angle=acosd(dot(vec,vec_tail)/(norm(vec)*norm(vec_tail)));
+if w == 1
+    if abs(angle) < 5
+        if rand<0.5
+            vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)];
+            s=s+1;
+        else
+            vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
+        end
+    else if abs(angle) < 17
+            if s==1
+                vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)]; 
+            else
+                vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
+            end
+        else
+            if s==1
+                s=0;
+                vec=vec*[cosd(-6) sind(-6); -sind(-6) cosd(-6)];
+            else 
+                s=1;
+                vec=vec*[cosd(6) sind(6); -sind(6) cosd(6)];
+            end
+         end
+    end
+end
 pos_body=pos_body+0.1*vec;
+pos_head=pos_body+vec;
 if pos_head(1)>=650
     pos_head(1)=650;
     pos_head(2)=pos_head(2)+(vec(2)/vec(2)*2);
@@ -127,11 +94,30 @@ if pos_body(2)<=1
     pos_body(2)=1;
     pos_body(1)=pos_body(1)+(vec(1)/vec(1)*2);
 end
+%plot(pos_body(1),pos_body(2),'ob');hold on;
 vec_tail=vec;
 per=cat(2,per,perception(pos_head,con));
 con=a(floor(pos_head(1)),floor(pos_head(2)));
 run_terminate_base=0.148;
 t_run_kernel=0;
+weathervane_termination_base=2;
+    t_weathervane_kernel=0;
+    for i = 0:t_w
+        if i<=1
+            t_weathervane_kernel=t_weathervane_kernel+per(t-i+1)*85;
+        else if i<=10
+            t_weathervane_kernel=t_weathervane_kernel+per(t-i+1)*(-5);
+            end
+        end
+    end
+    r_weathervane_terminate=weathervane_termination_base+t_weathervane_kernel;
+    p_weathervane_terminate=r_weathervane_terminate*0.1;
+    t_w=t_w+1;
+    if rand<p_weathervane_terminate
+    w=0;
+    t_w=0;
+    s=0;
+    end
 for i = 0:t_run
     t_run_kernel=t_run_kernel+per(t-i+1)*(-1+i/10);
     %plot_kernal(i+1)=(-1+i/10);
--- turnRight.m
+++ turnRight.m
@@ -4,12 +4,12 @@ angle=acosd(dot(vec,vec_tail)/(norm(vec)*norm(vec_tail)));
 if angle>=120
     recenter(pos_body,vec_tail,vec,con,per,t_turn,t,1,angle);
 end
-new_vec=vec*[cosd(24) -sind(24); sind(24) cosd(24)];
-pos_head=pos_body+new_vec;
+vec=vec*[cosd(24) -sind(24); sind(24) cosd(24)];
+pos_head=pos_body+vec;
 if pos_head(1)>=650 || pos_head(1)<=0 || pos_head(2)>=1000 || pos_head(2)<=0
     recenter(pos_body,vec_tail,vec,con,per,t_turn,t,1,angle);
 end
-plot(pos_head(1),pos_head(2),'--xr'); hold on;
+%plot(pos_head(1),pos_head(2),'--xr'); hold on;
 per=cat(2,per,perception(pos_head,con));
 con=a(floor(pos_head(1)),floor(pos_head(2)));
 turn_terminate_base=2;
@@ -25,9 +25,9 @@ p_turn_terminate=r_turn_terminate*0.1;
 t=t+1;
 t_turn=t_turn+1;
 if rand<p_turn_terminate && angle > 37
-    run(pos_body,pos_head,vec_tail,new_vec,con,per,0,0,0,0,t);
+    run(pos_body,pos_head,vec_tail,vec,con,per,0,0,0,0,t);
 else
-turnRight(pos_body,pos_head,vec_tail,new_vec,con,per,t_turn,t);
+turnRight(pos_body,pos_head,vec_tail,vec,con,per,t_turn,t);
 end
 end
 
